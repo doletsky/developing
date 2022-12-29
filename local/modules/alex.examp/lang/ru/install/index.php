0980911b52512db9fee9a9868845d3f4ed2c0dd6
Alexander Doletsky <doletsky@bk.ru>
2022-12-29 12:17:00 +0300

    Debbuging exam module(install, uninstall)

--- local/modules/alex.examp/lang/ru/install/index.php
+++ local/modules/alex.examp/lang/ru/install/index.php
@@ -5,6 +5,7 @@
  * Date: 28.12.2022
  * Time: 10:42
  */
-$MESS['MYMODULE_MODULE_NAME']='Пример тестового модуля';
+$MESS['MYMODULE_MODULE_NAME']='Тестовый пример модуля';
 $MESS['MYMODULE_MODULE_DESCRIPTION']='Модуль для отработки';
 $MESS['MYMODULE_MODULE_PARTNER_NAME']='Alex';
+$MESS['FORM_INSTALL_TITLE']='Installing examp module';
